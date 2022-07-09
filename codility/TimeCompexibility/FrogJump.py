d44f0a47277c4571c25b49bba81b325fa8c43849
minj-L <52521457+minj-L@users.noreply.github.com>
2022-07-09 22:27:13 +0900

    Update FrogJump.py

--- codility/TimeCompexibility/FrogJump.py
+++ codility/TimeCompexibility/FrogJump.py
@@ -7,3 +7,15 @@ def solution (X, Y, D):
 print(solution(10, 85, 30))
 
 #오류 많음 11% 나옴
+
+def solution (X, Y, D):
+    des = Y - X # 개구리가 목표 지점 까지 가기위해 뛰어야 하는 거리
+    Q = des // D
+    R = des % D
+
+    if R == 0:
+        return Q
+    elif R < D:
+        return Q + 1
+
+print(solution(10, 85, 30))
