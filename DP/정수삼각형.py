2f566e7328ec09ee4ea71f3e85cf21d0145120b8
minj-L <52521457+minj-L@users.noreply.github.com>
2022-12-30 23:56:09 +0900

    Update 정수삼각형.py

--- DP/정수삼각형.py
+++ DP/정수삼각형.py
@@ -1,9 +1,11 @@
-def solution(triangle):
-    dp = [[0] * len(triangle) for _ in range(len(triangle))]
-    dp[0][0] = triangle[0][0]
+def solution(tri):
+    answer = 0
+    #1. 우선 배열을 초기화 한다
+    dp = [[0] * len(tri) for _ in range(len(tri))]
+    dp[0][0] = tri[0][0]
+    for i in range(len(tri) - 1):
+        for j in range(0, len(tri) - 1):
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + tri[i+1][j])
+            dp[i + 1][j + 1] = max(dp[i + 1][j + 1], dp[i][j] + tri[i + 1][j + 1])
 
-    for i in range(0, len(triangle) - 1):
-        for j in range(len(triangle[i])):
-            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j] + triangle[i + 1][j])
-            dp[i + 1][j + 1] = max(dp[i+1][j+1], dp[i][j] + triangle[i+1][j+1])
     return max(dp[-1])
