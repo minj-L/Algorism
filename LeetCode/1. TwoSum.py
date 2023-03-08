f024ed6893165a7838f6c76f32a64d35a1f7aa80
minj-L <52521457+minj-L@users.noreply.github.com>
2023-03-08 18:27:52 +0900

    Create 1. TwoSum.py

---
+++ LeetCode/1. TwoSum.py
@@ -0,0 +1,26 @@
+# 1 Try
+from itertools import combinations
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        answer = []
+        comb = list(combinations(nums, 2))
+        for i in comb:
+            if sum(i) == target:
+               for j in i:
+                   id = nums.index(j)
+                   print(id)
+                   answer.append(id)
+        return answer
+      
+# 2 Try
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        answer = []
+        for i in range(0, len(nums) - 1):
+            for j in range(i+1, len(nums)):
+                if (nums[i] + nums[j]) == target:
+                    answer.append(i)
+                    answer.append(j)
+                    break
+        return answer
