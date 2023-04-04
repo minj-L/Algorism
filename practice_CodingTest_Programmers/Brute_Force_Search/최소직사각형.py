240b646992db145430c7772f532c44fe80ba2a68
minj-L <52521457+minj-L@users.noreply.github.com>
2023-04-05 00:33:44 +0900

    Create 최소직사각형.py

---
+++ practice_CodingTest_Programmers/Brute_Force_Search/최소직사각형.py
@@ -0,0 +1,14 @@
+def solution(sizes):
+    
+    tmp = 0
+    new_c, new_r = [], []
+    for c, r in sizes:
+        if r > c:
+            tmp = c
+            c = r
+            r = tmp
+        new_c.append(c)
+        new_r.append(r)
+    
+    answer = max(new_c) * max(new_r)
+    return answer
