9f6e3666a9579681a9e555ff4679c5b47a78b345
minj-L <52521457+minj-L@users.noreply.github.com>
2023-04-05 00:16:45 +0900

    Delete stack&queue directory

--- stack&queue/같은숫자는싫어.py
+++
@@ -1,8 +0,0 @@
-def solution(arr):
-    stack = []
-    for i in range(len(arr)):
-        if i == 0:
-            stack.append(arr[i])
-        elif arr[i] != arr[i-1]:
-            stack.append(arr[i])
-    return stack
