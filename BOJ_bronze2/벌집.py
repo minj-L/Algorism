04a4d46cdd5b8b34f6ad0632947ee4cb7ace5b61
minj-L <52521457+minj-L@users.noreply.github.com>
2023-04-27 21:28:19 +0900

    Create 벌집.py

---
+++ BOJ_bronze2/벌집.py
@@ -0,0 +1,12 @@
+def solution(n):
+    answer = 0
+    beehouse = 1
+    how_room = 1
+
+    while n > beehouse:
+        beehouse += 6 * how_room
+        how_room += 1
+
+    return how_room
+
+print(solution(13))
