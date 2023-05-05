86588e0aff9fa94bb241eac1225826b12eab0ecb
minj-L <52521457+minj-L@users.noreply.github.com>
2023-05-05 19:46:50 +0900

    Create 최대공약수와최소공배수.py

---
+++ BOJ_bronze1/최대공약수와최소공배수.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+num1, num2 = map(int, input().split())
+
+r_num1 = num1
+r_num2 = num2
+
+res = []
+
+# 최대공약수
+while num2 > 0:
+    tmp = num1
+    num1 = num2
+    num2 = tmp % num2
+res.append(num1)
+    
+# 최소공배수
+lcm = (r_num1 * r_num2) // num1
+res.append(lcm)
+
+for i in res:
+    print(i)
