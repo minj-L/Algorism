d53a712a2d45a491de874447c8c7422fed94249a
minj-L <52521457+minj-L@users.noreply.github.com>
2023-05-07 23:26:44 +0900

    Update 좌표정렬하기.py

--- BOJ_silver5/좌표정렬하기.py
+++ BOJ_silver5/좌표정렬하기.py
@@ -3,3 +3,14 @@ def solution(n, arr):
     answer = sorted(arr, key=lambda x : x[1])
     return answer
 print(solution(5, [[3, 4],[1,1],[1,-1],[2,2],[3,3]]))
+# 테케는 맞았는데 틀렸다고나옴 왜..?
+
+import sys
+n=int(sys.stdin.readline())
+li=[]
+for i in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+    li.append([a, b])
+li.sort()
+for i in li:
+    print(i[0], i[1])
