9f03d838f69f4ba9ddbeec6eb8b9e15d00fab622
minj-L <52521457+minj-L@users.noreply.github.com>
2023-05-13 00:21:32 +0900

    Create 요세푸스문제.py

---
+++ BOJ_silver5/요세푸스문제.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def solution(n, k):
+    answer = []
+
+    queue = deque()
+
+    for i in range(1, n + 1):
+        queue.append(i)
+
+    while queue:
+        for i in range(k-1):
+            queue.append(queue.popleft())
+        answer.append(queue.popleft())
+    
+    print("<", end='')
+    for i in range(len(answer) - 1):
+        print("%d, "%answer[i], end='')
+    print(answer[-1], end='')
+    print(">")
+    return answer
+print(solution(7, 3))
