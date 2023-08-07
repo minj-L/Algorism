f8831d3b04f47b9589246e2b31bacc91eea6ef7a
minj-L <wonder_minji@naver.com>
2023-08-07 22:44:36 +0900

    코드 설명 주석 추가

--- programmers_lv3/정수삼각형.py
+++ programmers_lv3/정수삼각형.py
@@ -2,11 +2,11 @@ def solution(triangle):
     
     for i in range(1, len(triangle)):
         for j in range(i+1):
-            if j==0:
+            if j==0: # 숫자가 가장 왼쪽에 있을 때
                 triangle[i][j] += triangle[i-1][j]
-            elif j==i:
+            elif j==i: # 숫자가 가장 오른쪽에 있을 때
                 triangle[i][j] += triangle[i-1][-1]
-            else:
+            else: # 가운데 부분에 있을 때
                 triangle[i][j] += max(triangle[i-1][j-1], triangle[i-1][j])
 
     return max(triangle[-1])
