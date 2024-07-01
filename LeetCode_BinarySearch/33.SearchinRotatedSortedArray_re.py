4230693dd92d78b6e568ffe9ddb14d5454ec6f85
minj-L <wonder_minji@naver.com>
2024-07-01 22:47:14 +0900

    06월의 언젠가, 0701 풀이

---
+++ LeetCode_BinarySearch/33.SearchinRotatedSortedArray_re.py
@@ -0,0 +1,21 @@
+def search(nums, target):
+    start = 0
+    end = len(nums) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if target == nums[mid]:
+            return mid
+        
+        if nums[start] <= nums[mid]:
+            if target >= nums[start] and target < nums[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else:
+            if target > nums[mid] and target <= nums[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+    return -1
