6efd2907dc3c67fe810a35e38b8b3de88d101c1d
minj-L <wonder_minji@naver.com>
2024-07-02 22:44:29 +0900

    0702_풀이

---
+++ LeetCode_BinarySearch/33.re.py
@@ -0,0 +1,20 @@
+def solution(nums, target):
+    start = 0
+    end = len(nums) - 1
+
+    while start <= end:
+        mid = (start + end) // 2
+
+        if nums[mid] == target:
+            return mid
+        
+        if nums[start] <= nums[mid]:
+            if target >= nums[start] and target < nums[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        else:
+            if target > nums[mid] and target <= nums[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
