a624c7adfd07d4c9983e7f25136d089e818fc9dd
minj-L <wonder_minji@naver.com>
2024-11-19 21:24:11 +0900

    1119_풀이 수정중

--- LeetCode/26. Remove Duplicates from Sorted Array.py
+++ LeetCode/26. Remove Duplicates from Sorted Array.py
@@ -1,17 +1,24 @@
 # 딕셔너리로 풀어버리고 싶다 키값만 가져다가 세면 안돼?
 def solution(nums):
-    answer = 0
-    dic = {}
+    # answer = 0
+    # dic = {}
 
-    for i in nums:
-        if i in dic:
-            dic[i] += 1
-        else:
-            dic[i] = 1
+    # for i in nums:
+    #     if i in dic:
+    #         dic[i] += 1
+    #     else:
+    #         dic[i] = 1
 
-    for i in dic:
-        answer += 1
+    # for i in dic:
+    #     answer += 1
 
-    return int(answer)
+    # return int(answer)
+
+    if len(nums) == 0:
+        return 0
+    
+    for i in range(1, len(nums)):
+        
+
 
 print(solution([0,0,1,1,1,2,2,3,3,4]))
